4308fb66522ac2b401643f64b5bb0b9ad056cfb3
Charles Ivia <iviacharles@gmail.com>
2020-07-13 13:58:50 +0300

    Created Todo component

--- src/App.js
+++ src/App.js
@@ -14,10 +14,15 @@ function App() {
     },
   ]);
 
-  
+  const Todo = ({ todo }) => <div className="todo">{todo.text}</div>;
+
   return (
-    <div className="App">
-     
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+      </div>
     </div>
   );
 }
