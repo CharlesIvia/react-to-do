61c9527ce50ef126a9990fb7dac2542b97cea6e2
Charles Ivia <iviacharles@gmail.com>
2020-07-13 13:51:31 +0300

    Added state to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Learn React Hooks",
+    },
+    {
+      text: "Play soccer and exercise.",
+    },
+    {
+      text: "Read the Bible.",
+    },
+  ]);
+
+  
   return (
     <div className="App">
-      <h1>Hello World.</h1>
+     
     </div>
   );
 }
